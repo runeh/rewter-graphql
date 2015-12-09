3e83696d1de4ba6927b6baf1d9e374e4ab85a38c
Rune Halvorsen <runefh@gmail.com>
2015-12-09 15:49:52 +0100

    Cleaned up main a bit

--- main.js
+++ main.js
@@ -1,31 +1,8 @@
-
 import {graphql} from 'graphql';
 import {schema} from './schema';
-import {stopInfo, lineInfo} from './ruter-fetcher';
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
 
- var query = `
-        query IntrospectionTypeQuery {
-          __schema {
-            types {
-              name
-            }
-          }
-        }
-      `;
-
-// graphql(schema, query).then(e => {
-//     console.log(JSON.stringify(e, null, 4))
-
-// })
-
-// console.log("slsls")
-// stopInfo("3010050").then(console.log).catch(console.log)
-// getStopsByLineId("18").then(console.log).catch(console.log)
-// var graphqlHTTP = require('express-graphql');
-
 const app = express();
 app.use('/graphql', graphqlHTTP({ schema: schema, graphiql: true }));
-
-app.listen(3011)
+app.listen(3011);
