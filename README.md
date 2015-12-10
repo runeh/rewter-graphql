dd750af646ad3c0a575a27f857851515adb5b4b2
runhalvo <rune.halvorsen@finn.no>
2015-12-10 15:15:04 +0100

    Pre publish cleanup

---
+++ README.md
@@ -0,0 +1,6 @@
+# rewter
+
+Graphql for ruter's public transit APIs.
+
+npm install; npm start
+
--- main.js
+++ main.js
@@ -4,5 +4,9 @@ import express from 'express';
 import graphqlHTTP from 'express-graphql';
 
 const app = express();
-app.use('/graphql', graphqlHTTP({ schema: schema, graphiql: true }));
-app.listen(3011);
+app.set('port', (process.env.PORT || 3011));
+app.use('/', graphqlHTTP({ schema: schema, graphiql: true }));
+
+app.listen(app.get('port'), function() {
+    console.log('rewter is running on port', app.get('port'));
+});
