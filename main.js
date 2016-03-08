3d964d48fc6077b36aa2b11dcb89a4080ddf44f6
Rune Halvorsen <runefh@gmail.com>
2016-03-08 09:13:40 +0100

    Formaterror setting for graphiql

--- main.js
+++ main.js
@@ -5,7 +5,11 @@ import graphqlHTTP from 'express-graphql';
 
 const app = express();
 app.set('port', (process.env.PORT || 3011));
-app.use('/', graphqlHTTP({ schema: schema, graphiql: true }));
+app.use('/', graphqlHTTP({
+	schema: schema,
+	graphiql: true,
+	formatError: error => error
+}));
 
 app.listen(app.get('port'), function() {
     console.log('rewter is running on port', app.get('port'));
